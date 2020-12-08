257b5f5e903e18d039c6fdb66b7f2cc15875fe51
santiagorodriguez-manzaneque <santiagormj@icloud.com>
2020-12-08 20:52:40 +0100

    login funcionando

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,22 +3,26 @@ import React, {useState } from 'react';
 import { Input } from 'antd';
 import './Login.scss'
 import { useHistory } from 'react-router-dom';
+import {notification} from 'antd'
 
 
 
-const LoginForm = (props) => {
+const LoginForm = () => {
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('');
+    const [pass, setPass] = useState('')
     const URL = 'https://moviedb-mongo.herokuapp.com/user/login'
     const history = useHistory();
     const login = async (e) => {
         try {
             e.preventDefault();
-            const post = await axios.post(URL, {email,pass})
+            const post = await axios.post(URL, {name,email,pass})
             localStorage.setItem('token',post.data.token)
-            //props.setUser(post.data.user)
             console.log(post)
-            //history.push('/')
+            notification['success']({
+                message: "Login correcto!"
+            })
+            history.push('/')
         } catch (error) {
             console.error(error)
         }
@@ -26,8 +30,9 @@ const LoginForm = (props) => {
     }
     return (
         <form className="Login" onSubmit={login}>
-            <Input type="email" onChange={e=>setEmail(e.target.value)} name="email" placeholder="Email" value={email} className="input-1"/>
-            <Input type="password" onChange={e=>setPass(e.target.value)} name="password" placeholder="Contraseña" value={pass} className="input-1"/>
+            <Input type="text" onChange={e=>setName(e.target.value)} name="name" placeholder="Nombre" value={name} className="loginForm_input"/>
+            <Input type="email" onChange={e=>setEmail(e.target.value)} name="email" placeholder="Email" value={email} className="loginForm_input"/>
+            <Input type="password" onChange={e=>setPass(e.target.value)} name="password" placeholder="Contraseña" value={pass} className="loginForm_input"/>
             <button type="submit" className="button">Enviar</button>
         </form>
     )
